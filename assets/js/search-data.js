d977b5e0c89ebbaad5cd6b1ded5a507183a51dc3
HuaYangttt <145650058+HuaYangttt@users.noreply.github.com>
2025-09-24 01:22:54 +0000

    Deploying to gh-pages from @ HuaYangttt/HuaYangttt.github.io@5a400eca86e58b6b95bcf34436b974f9e2640965 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,51 +408,21 @@ ninja.data = [{
             },},{id: "news-start-my-phd-study-at-ncsu",
           title: 'start my PhD study at NCSU',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-evading-membership-inference-in-code-llms-via-semantically-equivalent-transformations",
+          title: 'Evading Membership Inference in Code LLMs via Semantically Equivalent Transformations',
+          description: "Investigating transformation-based obfuscation and its impact on license compliance enforcement",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-movie-recommendation-web-app-integrating-llms",
+          title: 'Movie Recommendation Web App Integrating LLMs',
+          description: "AI-driven movie recommendation system with collaborative development",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-privacy-risks-of-personally-identifiable-information-in-llms4code",
+          title: 'Privacy Risks of Personally Identifiable Information in LLMs4Code',
+          description: "Causal analysis of heterogeneous PII leakage risks in LLMs for Code",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
